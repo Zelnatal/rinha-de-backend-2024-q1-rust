5264c1edb71137a015dfa61deb2bb624b5e141b0
= <=>
2024-03-03 12:36:52 -0300

    Algumas mudanças

--- rust.Dockerfile
+++ rust.Dockerfile
@@ -1,15 +1,13 @@
-FROM rust:1.76-alpine as builder
+FROM rust:1.76 as builder
 WORKDIR /app
 COPY ./Cargo.toml .
 COPY ./Cargo.lock .
 COPY ./src ./src
 COPY ./.sqlx ./.sqlx
 
-RUN apk add --no-cache musl-dev
-
 RUN cargo build --release
 
-FROM alpine:latest
+FROM debian:12.5
 WORKDIR /app
 COPY --from=builder /app/target/release/rinha-backend-2024-q1-rust .
 ENTRYPOINT [ "./rinha-backend-2024-q1-rust" ]
--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,18 @@ mod routes;
 async fn main() {
     let app = create_app().await;
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener,app).await.unwrap();
+
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown())
+        .await
+        .unwrap();
+}
+
+async fn shutdown() {
+    let _ = tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+        .unwrap()
+        .recv()
+        .await
+        .unwrap();
+    todo!()
 }
